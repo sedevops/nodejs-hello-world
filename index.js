b6fdb42dc5dd3190a688faf3d9c64d797373c888
sedevops <40681393+sedevops@users.noreply.github.com>
2019-01-14 21:41:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Are you seeing the changes with updates 1?");
+    response.end("Are you seeing the changes with updates 2?");
 
 });
 
