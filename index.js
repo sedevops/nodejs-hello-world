43d4b0ce1049bae837af010c8c6624158aba4183
sedevops <40681393+sedevops@users.noreply.github.com>
2019-01-14 16:45:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello from PushTrain - Testing blue green deployment updated");
+    response.end("Are you seeing the changes?");
 
 });
 
