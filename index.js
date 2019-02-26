25f4bb56de887189f80013be7387da41c9f90545
sedevops <40681393+sedevops@users.noreply.github.com>
2019-02-26 14:30:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Zero downtime deployment with vars:" + process.env.DEMO);
+    response.end("Zero downtime deployment with vars: Hello" + process.env.DEMO);
 
 });
 
